73a5f6b84bf63e77602c1729a6334ba391daf4eb
Naililruojnob <146110607+Naililruojnob@users.noreply.github.com>
2025-02-19 11:40:50 +0100

    Update kube-workers-ufw.sh

--- ufw/kube-workers-ufw.sh
+++ ufw/kube-workers-ufw.sh
@@ -10,8 +10,9 @@ else
     echo "UFW est déjà installé."
 fi
 
-# Définir la liste des masters autorisés (remplace par les IPs de tes Masters)
-MASTERS=("IP")
+# Définir la liste des masters et workers autorisés (remplace par les IPs de tes Masters et Workers)
+MASTERS=("IP_MASTER_1" "IP_MASTER_2")
+WORKERS=("IP_WORKER_1" "IP_WORKER_2")
 
 # Réinitialiser UFW et appliquer une politique par défaut stricte
 ufw --force reset
@@ -19,9 +20,12 @@ ufw default deny incoming  # Bloquer tout le trafic entrant par défaut
 ufw default allow outgoing  # Autoriser tout le trafic sortant par défaut
 
 # Autoriser kubelet API (auto-administré par le nœud lui-même et accessible par le control plane)
-for MASTERS in "${MASTERS[@]}"; do
-	ufw allow proto tcp from $MASTERS to any port 10250 comment "Autoriser kubelet API pour le control plane"
+
+# Autoriser les connexions depuis les masters et workers
+for IP in "${MASTERS[@]}" "${WORKERS[@]}"; do
+    ufw allow from $IP comment "Autoriser les connexions depuis $IP"
 done
+
 ## (optionel) Autoriser les NodePort Services pour les communications internes
 # ufw allow proto tcp from any to any port 30000:32767 comment "Autoriser NodePort Services"
 
